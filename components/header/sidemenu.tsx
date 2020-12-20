12fa1d0e00e074aa692a4c28efe9bbcf33f4ac84
Daigoshimomura <barumunku23@yahoo.co.jp>
2020-12-20 15:32:46 +0900

    refacter:コンポーネント見直し

--- components/header/sidemenu.tsx
+++ components/header/sidemenu.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { MenuButton, Menu, RankList } from '../button/menu';
+import rank from '../../public/json/rank.json';
 
 type Props = {
   className?: string;
@@ -10,22 +10,36 @@ type Props = {
 const Base: React.FC<Props> = ({ className }) => {
   //WinRateflag
   const [isWinOpen, setIsWinOpen] = useState(false);
+  //rank一覧
+  const rankList = rank.rank.map((elm, index) => {
+    return (
+      <li key={index} className={`${className}__rankList`}>
+        <img
+          src={`rank/${elm.key}.png`}
+          alt={elm.name}
+          className={`${className}__rankPng`}
+        />
+        <a>{elm.name}</a>
+      </li>
+    );
+  });
 
   return (
     <div className={`${className}`}>
       <div className={`${className}__menu`}>
         <div className={`${className}__flame`}>
           <Link href={`/`}>
-            <MenuButton>Home</MenuButton>
+            <a className={`${className}__menuButton`}>Home</a>
           </Link>
         </div>
         <div className={`${className}__flame`}>
           <Link href={`/`}>
-            <MenuButton>Team Building</MenuButton>
+            <a className={`${className}__menuButton`}>Team Building</a>
           </Link>
         </div>
         <div className={`${className}__flame`}>
-          <Menu
+          <div
+            className={`${className}__menuList`}
             onClick={() => {
               setIsWinOpen(!isWinOpen);
             }}
@@ -35,14 +49,10 @@ const Base: React.FC<Props> = ({ className }) => {
               className={`${className}__arrow`}
               src={`/button/arrow-down.png`}
             />
-          </Menu>
+          </div>
         </div>
         {isWinOpen ? (
-          <ul className={`${className}__flameList`}>
-            <RankList>
-              <a>Challenger</a>
-            </RankList>
-          </ul>
+          <ul className={`${className}__flameList`}>{rankList}</ul>
         ) : null}
       </div>
     </div>
@@ -66,6 +76,52 @@ const Header = styled(Base)`
     border-bottom: 1px solid;
     border-color: #8c8c8c;
   }
+  &__menuButton {
+    font-size: 18px;
+    color: #b2b2b2;
+    text-decoration: none;
+    position: relative;
+    display: inline-block;
+    transition: 0.1s;
+    cursor: pointer;
+    ::after {
+      position: absolute;
+      bottom: 0;
+      left: 50%;
+      content: '';
+      width: 0;
+      height: 2px;
+      background-color: #b2b2b2;
+      transition: 0.3s;
+      transform: translateX(-50%);
+    }
+    :hover::after {
+      width: 100%;
+    }
+  }
+  &__menuList {
+    font-size: 18px;
+    color: #b2b2b2;
+    text-decoration: none;
+    position: relative;
+    display: inline-block;
+    transition: 0.1s;
+    cursor: pointer;
+    ::after {
+      position: absolute;
+      bottom: 0;
+      left: 50%;
+      content: '';
+      width: 0;
+      height: 2px;
+      background-color: #b2b2b2;
+      transition: 0.3s;
+      transform: translateX(-50%);
+    }
+    :hover::after {
+      width: 100%;
+    }
+  }
   &__flameList {
     list-style: none;
     margin-top: 5px;
@@ -73,6 +129,24 @@ const Header = styled(Base)`
   }
   &__arrow {
     margin-left: 82px;
+  }
+  &__rankPng {
+    height: 20px;
+    width: 23px;
+  }
+  &__rankList {
+    display: inline-block;
+    font-size: 18px;
+    font-weight: bold;
+    height: 32px;
+    width: 149px;
+    border-radius: 10px;
+    color: #ffffff;
+    background-color: #5987cd;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-bottom: 5px;
   }
 `;
 
